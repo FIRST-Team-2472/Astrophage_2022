6f721268a4a99227073dbce4cdd35019d8ffdaa1
ModernTinTin1 <maximus.theis@gmail.com>
2021-12-03 11:49:23 -0600

    Action Queue has been made

--- src/main/java/frc/robot/ActionQueue/Runners/ActionQueue.java
+++ src/main/java/frc/robot/ActionQueue/Runners/ActionQueue.java
@@ -1,5 +1,44 @@
 package frc.robot.ActionQueue.Runners;
 
+import java.util.ArrayList;
+
 public class ActionQueue {
-    
+    private ArrayList<Actionable> queue;
+    boolean inProgress = true;
+
+    public ActionQueue() {
+        queue = new ArrayList<Actionable>();
+    }
+
+    public void addAction(Actionable action) {
+        queue.add(action);
+    }
+
+    public void step() {
+        if (!queue.isEmpty()) {
+            Actionable action = queue.get(0);
+
+            action.periodic();
+
+            inProgress = true;
+
+            if (action.isFinished()) {
+                action.endAction();
+
+                queue.remove(0);
+                action = queue.get(0);
+                action.startAction();
+            }
+        } else inProgress = false;
+    }
+
+    public void clear() {
+        for (int i = 0; i < queue.size(); i++) {
+            queue.remove(i);
+        }
+    }
+
+    public boolean isInProgress() {
+        return inProgress;
+    }
 }
