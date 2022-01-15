dbe37ed335292367387f25be96917233fb92a486
ModernTinTin1 <sawyer.c.theis@gmail.com>
2022-01-15 10:25:49 -0600

    Climber Mk2

---
+++ src/main/java/frc/robot/Subsystems/ClimberMove.java
@@ -0,0 +1,8 @@
+package frc.robot.Subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class ClimberMove {
+    private TalonSRX climberMoveOne;
+    private TalonSRX climberMoveTwo;
+}
