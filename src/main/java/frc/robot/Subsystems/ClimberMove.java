d69a5acc06bd12ef7f4642808344954c8a69b23b
Hoplite30000 <97264604+Hoplite30000@users.noreply.github.com>
2022-01-15 10:58:29 -0600

    more subsystems
    climby boi go weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- src/main/java/frc/robot/Subsystems/ClimberMove.java
+++ src/main/java/frc/robot/Subsystems/ClimberMove.java
@@ -1,8 +1,32 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-public class ClimberMove {
-    private TalonSRX climberMoveOne;
-    private TalonSRX climberMoveTwo;
+public class ClimberMove 
+{
+    private TalonSRX climberMoveEins;
+    private TalonSRX climberMoveZwei;
+
+    public ClimberMove (int climberMoveEinsID, int climberMoveZweiID)
+    {
+      climberMoveEins = new TalonSRX(climberMoveEinsID);
+      climberMoveZwei = new TalonSRX(climberMoveZweiID);
+    }
+    
+    public void runclimberMoveEins(double speed)
+  {
+    climberMoveEins.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void runclimberMoveZwei(double speed)
+  {
+    climberMoveZwei.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void runclimberMoveBoth(double speed)
+  {
+    climberMoveEins.set(ControlMode.PercentOutput, speed);
+    climberMoveZwei.set(ControlMode.PercentOutput, speed);
+  }
 }
