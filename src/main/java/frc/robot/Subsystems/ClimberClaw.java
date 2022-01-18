e619bec136a084027ff93b8dfcd9bf6cf06c0812
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2022-01-18 15:33:30 -0600

    Ja

--- src/main/java/frc/robot/Subsystems/ClimberClaw.java
+++ src/main/java/frc/robot/Subsystems/ClimberClaw.java
@@ -1,8 +1,32 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-public class ClimberClaw {
-    private TalonSRX climberClawOne;
-    private TalonSRX climberClawTwo;
+public class ClimberClaw 
+{
+  private TalonSRX climberClawEins;
+  private TalonSRX climberClawZwei;
+
+  public ClimberClaw (int climberClawEinsID, int climberClawZweiID)
+  {
+    climberClawEins = new TalonSRX(climberClawEinsID);
+    climberClawZwei = new TalonSRX(climberClawZweiID);
+  }
+  
+  public void runclimberClawEins(double speed)
+  {
+    climberClawEins.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void runclimberClawZwei(double speed)
+  {
+    climberClawZwei.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void runclimberClawBoth(double speed)
+  {
+    climberClawEins.set(ControlMode.PercentOutput, speed);
+    climberClawZwei.set(ControlMode.PercentOutput, speed);
+  }
 }
