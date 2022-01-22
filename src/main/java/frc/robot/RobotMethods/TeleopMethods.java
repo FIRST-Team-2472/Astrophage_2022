5dfdc9be6d60f41f786bd75e84abbe98d344b9aa
ModernTinTin1 <sawyer.c.theis@gmail.com>
2022-01-22 13:33:30 -0600

    issue resolves

--- src/main/java/frc/robot/RobotMethods/TeleopMethods.java
+++ src/main/java/frc/robot/RobotMethods/TeleopMethods.java
@@ -1,37 +1,81 @@
 package frc.robot.RobotMethods;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
-public class TeleopMethods {
+
+
+
+public class TeleopMethods 
+{
+    boolean intakeWheel = false;
+    boolean clamp = false;
     
-    public void init() {
+    public void init() 
+    {
         //Displays a value to ShuffleBoard
         SmartDashboard.putString("RobotState", "Telop Enabled");
         Robot.drive.zeroEncoders();
     }
 
 
-    public void drive() {
+    public void drive() 
+    {
         Robot.drive.arcadeDrivePower(Robot.leftJoystick.getY()*.5, Robot.leftJoystick.getX()*.5);
     }
 
-    public void aimForBall() {
+    public void intake()
+    {
 
-        double limelightkP = 0.02;
-     
-			Robot.drive.arcadeDrivePower(Robot.leftJoystick.getY()*.5, (-1 * (limelightkP * Robot.limelight.targetXAngleFromCenter())));
+        if (Robot.xboxcontroller.getAButton()) Robot.intake.runFrontWheels(.5);
+        else Robot.intake.runFrontWheels(0);
+
+        if (Robot.xboxcontroller.getYButtonPressed())
+        {
+            if (intakeWheel == false) intakeWheel = true;
+            else intakeWheel = false;
             
+            if (intakeWheel == false) Robot.intake.pushinfrontwheel();
+            else Robot.intake.pushoutfrontwheel();
+        }
 
+
+
+        if (Robot.xboxcontroller.getBButton()) Robot.intake.runConveyorPower(.5);
+        else Robot.intake.runConveyorPower(0);
+    }
+
+    public void shooter()
+    {
+        if (Robot.xboxcontroller.getXButtonPressed()) Robot.shooter.runFlyWheelPower(.5);
+        else Robot.shooter.runFlyWheelPower(0);
+    }
+
+    public void climberClamp()
+    {
         
+        if (Robot.xboxcontroller.getRightBumper())
+        {
+            if (clamp == false) clamp = true;
+            else clamp = false;
+            
+            if (clamp == false) Robot.climberClamp.pushInClamps();
+            else Robot.climberClamp.pushUpClamps();
+        }
+    }
+    
+    public void climberClaw()
+    {
+        if (Robot.xboxcontroller.getPOV() == 0) Robot.climberClaw.runclimberClawBoth(.5);
+        else if (Robot.xboxcontroller.getPOV() == 180) Robot.climberClaw.runclimberClawBoth(-.5);
+        else Robot.climberClaw.runclimberClawBoth(0);
+    }
+
+    public void ClimberMove()
+    {
+        if (Robot.xboxcontroller.getRightTriggerAxis() >= .6) Robot.climberMove.runclimberMoveBoth(.5);
+        else if (Robot.xboxcontroller.getLeftTriggerAxis() >= .6) Robot.climberMove.runclimberMoveBoth(-.5);
+        else Robot.climberMove.runclimberMoveBoth(0);
     }
 }
-
-    /*public void shooting () {
-        if (Robot.xboxcontroller.getXButton()) {
-            Robot.shooter.runFlyWheel(1);
-
-        } else {
-            Robot.shooter.runFlyWheel(0);
-        }
-    }*/
