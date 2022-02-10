67b4b0c956444cf697acbb2cfa9499c1e11dcf68
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2022-02-10 17:09:57 -0600

    more motion magic

--- src/main/java/frc/robot/Subsystems/Drive.java
+++ src/main/java/frc/robot/Subsystems/Drive.java
@@ -8,16 +8,19 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Drive {
-    //Talon Definition: an instruction reader that gives instructions to the stupid motors after reading the instructions from the big instruction reader that reads instructions from the instruction manual written by the instruction man
+    // Talon Definition: an instruction reader that gives instructions to the stupid
+    // motors after reading the instructions from the big instruction reader that
+    // reads instructions from the instruction manual written by the instruction man
     private TalonFX rightMaster;
     private TalonFX rightSlave;
     private TalonFX leftMaster;
     private TalonFX leftSlave;
 
-    //TODO find speical number
+    // TODO find speical number
     private double countToFeet = 00000000000;
+    private double KP = 0, KF = 0, KI = 0;
 
-    //Assigns IDs talons to hand instructions to dum kopf motors
+    // Assigns IDs talons to hand instructions to dum kopf motors
     public Drive(int backRightID, int frontRightID, int backLeftID, int frontLeftID) {
         rightMaster = new TalonFX(backRightID);
         rightSlave = new TalonFX(frontRightID);
@@ -30,76 +33,21 @@ public class Drive {
 
         rightSlave.follow(rightMaster);
         rightSlave.setInverted(InvertType.FollowMaster);
-        //Slaves loyally mimic master's movements, including inversion
+        // Slaves loyally mimic master's movements, including inversion
         leftSlave.follow(leftMaster);
         leftSlave.setInverted(InvertType.FollowMaster);
 
+        setUpMotionMagicFX(leftMaster, KF, KP, KI);
+        setUpMotionMagicFX(rightMaster, KF, KP, KI);
 
-
-
-        rightMaster.config_kP(0, 0.05);
-        rightMaster.config_kI(0, 0);
-        rightMaster.config_kD(0, 0.05);
-        rightMaster.config_kF(0, -.19);
-    
-
-        //encoders
-        
-        rightMaster.setSensorPhase(true);  // correct encoder to motor direction
-     
-        // Tell the talon that he has a quad encoder
-        rightMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
-       
-        // Set minimum output (closed loop)  to 0 for now
-        rightMaster.configNominalOutputForward(0, 30);
-        rightMaster.configNominalOutputReverse(0, 30);
-        
-        // Set maximum forward and backward to full speed
-        rightMaster.configPeakOutputForward(1, 30);
-        rightMaster.configPeakOutputReverse(-1, 30);
-    
-        // Motion magic cruise (max speed) is 100 counts per 100 ms
-        rightMaster.configMotionCruiseVelocity(2000, 30);
-    
-        // Motion magic acceleration is 50 counts
-        rightMaster.configMotionAcceleration(2000, 30);
-    
-        // Zero the sensor once on robot boot up 
-        rightMaster.setSelectedSensorPosition(0, 0, 30);
-
-    //other side
-
-            leftMaster.setSensorPhase(true);  // correct encoder to motor direction
-
-            // Tell the talon that he has a quad encoder
-            leftMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
-           
-            // Set minimum output (closed loop)  to 0 for now
-            leftMaster.configNominalOutputForward(0, 30);
-            leftMaster.configNominalOutputReverse(0, 30);
-            
-            // Set maximum forward and backward to full speed
-            leftMaster.configPeakOutputForward(1, 30);
-            leftMaster.configPeakOutputReverse(-1, 30);
-        
-            // Motion magic cruise (max speed) is 100 counts per 100 ms
-            leftMaster.configMotionCruiseVelocity(2000, 30);
-        
-            // Motion magic acceleration is 50 counts
-            leftMaster.configMotionAcceleration(2000, 30);
-        
-            // Zero the sensor once on robot boot up 
-            leftMaster.setSelectedSensorPosition(0, 0, 30);
-        
-        
     }
 
-
     public void arcadeDrive(double y, double x) {
-        SmartDashboard.putNumber("hi",2);
+        SmartDashboard.putNumber("hi", 2);
         // y is the y axis of the joystick
         // x is the x axis of the SAME joystick
-        if (Math.abs(x) < .1) x = 0;
+        if (Math.abs(x) < .1)
+            x = 0;
 
         if (Math.abs(x) + Math.abs(y) < .75) {
             tankDrive(y - x, y + x);
@@ -121,8 +69,8 @@ public class Drive {
     }
 
     public void runFeetDrive(double feet) {
-        setLeftTarget(feet / countToFeet);
-        setRightTarget(feet / countToFeet);
+        setLeftTarget(feet);
+        setRightTarget(feet);
     }
 
     public void runLeft(double speed) {
@@ -134,14 +82,14 @@ public class Drive {
     }
 
     public void setLeftTarget(double feet) {
-        leftMaster.set(ControlMode.MotionMagic, feet * countToFeet);
+        leftMaster.set(ControlMode.MotionMagic, feet / countToFeet);
     }
 
     public void setRightTarget(double feet) {
-        leftMaster.set(ControlMode.MotionMagic, feet * countToFeet);
+        leftMaster.set(ControlMode.MotionMagic, feet / countToFeet);
     }
 
-    public void zeroEncoders(){
+    public void zeroEncoders() {
         rightMaster.setSelectedSensorPosition(0);
         leftMaster.setSelectedSensorPosition(0);
     }
@@ -163,48 +111,70 @@ public class Drive {
     }
 
 
+    private void setUpMotionMagicFX(TalonFX motor, double KF, double KP, double KI) {
+        motor.configFactoryDefault();
+
+        // kP formula is (x/1023)/4096
+        motor.config_kP(0, KP);
+        motor.config_kI(0, KI);
+        motor.config_kF(0, KF);
+
+        // D stands for don't touch
+        motor.config_kD(0, 0);
+
+        motor.setSensorPhase(true); // correct encoder to motor direction
+
+        // Set minimum output (closed loop) to 0 for now
+        motor.configNominalOutputForward(0, 30);
+        motor.configNominalOutputReverse(0, 30);
+
+        // Set maximum forward and backward to full speed
+        motor.configPeakOutputForward(1, 30);
+        motor.configPeakOutputReverse(-1, 30);
+
+        // Motion magic cruise (max speed) is 100 counts per 100 ms
+        motor.configMotionCruiseVelocity(2000, 30);
+
+        // Motion magic acceleration is 50 counts
+        motor.configMotionAcceleration(2000, 30);
+
+        // Zero the sensor once on robot boot up
+        motor.setSelectedSensorPosition(0, 0, 30);
+    }
 
 
+    
+    // power drive hopefully not have to use
 
-
-
-
-
-
-
-
-
-    //power drive hopefully not have to use
-
-    //Better tankDrive used exclussively with joystick(s)
+    // Better tankDrive used exclussively with joystick(s)
     public void arcadeDrivePower(double y, double x) {
-        //y is the y axis of the joystick
-        //x is the x axis of the SAME joystick
+        // y is the y axis of the joystick
+        // x is the x axis of the SAME joystick
 
         if (Math.abs(x) + Math.abs(y) < .75) {
             tankDrivePower(y + x, y - x);
         } else {
             // limits the motors from ever going over 75% speed
-            double betterX = (x/(Math.abs(x)+Math.abs(y)))*.75;
-            double betterY = (y/(Math.abs(x)+Math.abs(y)))*.75;
+            double betterX = (x / (Math.abs(x) + Math.abs(y))) * .75;
+            double betterY = (y / (Math.abs(x) + Math.abs(y))) * .75;
             tankDrivePower(betterY + betterX, betterY - betterX);
         }
     }
 
-    //Runs speed methods base upon two values
+    // Runs speed methods base upon two values
     public void tankDrivePower(double leftSpeed, double rightSpeed) {
         runLeftPower(leftSpeed);
         runRightPower(rightSpeed);
     }
 
-    //Commands leftMaster's speed with a percentage
+    // Commands leftMaster's speed with a percentage
     public void runLeftPower(double speed) {
         leftMaster.set(ControlMode.PercentOutput, speed);
     }
 
-    //Commands rightMaster's speed with a percentage
+    // Commands rightMaster's speed with a percentage
     public void runRightPower(double speed) {
         rightMaster.set(ControlMode.PercentOutput, speed);
     }
-    
+
 }
