d90998bad4fcfce0c944965519ba8d257e081bbd
Ben Wespiser <92940727+Wespiseb@users.noreply.github.com>
2022-02-12 12:29:48 -0600

    HSAf

--- src/main/java/frc/robot/Subsystems/Drive.java
+++ src/main/java/frc/robot/Subsystems/Drive.java
@@ -151,12 +151,12 @@ public class Drive {
         // x is the x axis of the SAME joystick
 
         if (Math.abs(x) + Math.abs(y) < .75) {
-            tankDrivePower(y + x, y - x);
+            tankDrivePower(y - x, y + x);
         } else {
             // limits the motors from ever going over 75% speed
             double betterX = (x / (Math.abs(x) + Math.abs(y))) * .75;
             double betterY = (y / (Math.abs(x) + Math.abs(y))) * .75;
-            tankDrivePower(betterY + betterX, betterY - betterX);
+            tankDrivePower(betterY - betterX, betterY + betterX);
         }
     }
 
