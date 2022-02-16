e97e310711aa4c6a8c6fded350a6b71e3575574d
Sawyer <58192423+BlackPowderPirate@users.noreply.github.com>
2022-02-15 18:07:09 -0600

    shuffleboard.png

--- src/main/java/frc/robot/Miscellaneous/ShuffleBoard.java
+++ src/main/java/frc/robot/Miscellaneous/ShuffleBoard.java
@@ -1,7 +1,6 @@
 package frc.robot.Miscellaneous;
 
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.HttpCamera;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -16,15 +15,21 @@ public class ShuffleBoard {
 
     private UsbCamera camera1;
     private UsbCamera camera2;
-    private HttpCamera limelightFeed;
+    private int cameraStream1 = 0;
+    private int cameraStream2 = 1;
+    private NetworkTableEntry robotState;
+    private ShuffleboardTab driverBoard;
     private ComplexWidget cameraDisplay1, cameraDisplay2;
 
     public ShuffleBoard() {
 
+        camera1 = CameraServer.startAutomaticCapture(0);
+        camera2 = CameraServer.startAutomaticCapture(1);
+
         
-        //driverBoard.addCamera("camera 1", "camera1", "mjpeg:http://roboRIO-2016-FRC.local:1181/?action=stream");
-        //driverBoard.addCamera("camera 2", "camera2", "mjpeg:http://roboRIO-2016-FRC.local:1182/?action=stream");
-        //driverBoard.addCamera("Limelight", "Limelight", ""http://limelight.local:5801/stream.mjpg");
+        robotState = driverBoard.add("Robot State", "").getEntry();
+        driverBoard.addCamera("camera 1", "camera1", "mjpeg:http://roboRIO-2016-FRC.local:1181/?action=stream");
+        driverBoard.addCamera("camera 2", "camera2", "mjpeg:http://roboRIO-2016-FRC.local:1182/?action=stream");
         
         
         
